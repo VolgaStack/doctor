bb9095f40c92a00a3f96ed204a0c2a5a44837af7
Dima <raziel.kdm@gmail.com>
2015-09-17 23:13:56 +0300

    Forgot forms.py
    forms.py -> add doctor field to form.

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from .models import Doctor
 
 class DoctorForm(forms.Form):
     name = forms.CharField(label='ФИО', max_length=200,)
     date = forms.DateField(label='Дата записи',)
+    doctor = forms.ModelChoiceField(queryset=Doctor.objects.all())
  
     
